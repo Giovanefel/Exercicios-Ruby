b048f7c933686d6f4a9d7a85384c032432601dd2
Giovane <giovanefe17@gmail.com>
2024-05-11 19:34:06 -0300

    Primeiro Hello World
    Primeiro passo em Ruby
    Hello world e perguntas básicas
    
    usando Print e Puts.

---
+++ hello.rb
@@ -0,0 +1,12 @@
+print 'HELLO WORLD'
+
+#print mostra o dados
+print 'digite seu nome?'
+
+nome = gets.chomp         #nome é var que recebe o nome de "nome"     gets recebe dados
+
+print 'digite seu sobrenome?'
+
+sobrenome = gets.chomp
+
+puts"hello #{nome} seu sobrenome é #{sobrenome}!"           #puts tambem mostra os dados     #{ }mostra a var no qual foi atribuido o "dados" ex nome e sobre nome
