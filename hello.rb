cf1ea37e11b67769fa4f8c4b821ff70deadce2a6
Giovane <giovanefe17@gmail.com>
2024-05-12 21:09:59 -0300

    cont
    apenas poucas modificações para familiarizar

--- hello.rb
+++ hello.rb
@@ -1,15 +1,15 @@
-print 'HELLO WORLD'
+print 'HELLO WORLD' "\n "
 
 #print mostra o dados
-print 'digite seu nome?'
+print 'Digite seu nome?'
 
 nome = gets.chomp         #nome é var que recebe o nome de "nome"     gets recebe dados
 
-print 'digite seu sobrenome?'
+print 'Digite seu sobrenome?'
 
 sobrenome = gets.chomp
 
-puts"hello #{nome} seu sobrenome é #{sobrenome}!"      
+puts"hello Sr.#{nome} seu sobrenome é #{sobrenome}!"      
 #puts tambem mostra os dados  
 #{ }mostra a var no qual foi atribuido o "dados" ex nome e sobrenome
-#criando comentario para ser atualizado no gitdesk
+#criando comentario para ser atualizado no gitdesk 
