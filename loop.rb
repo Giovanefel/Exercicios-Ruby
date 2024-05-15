1fa66f8262d94dd2efbbe19d7df0491febe21088
Giovane <giovanefe17@gmail.com>
2024-05-14 23:10:12 -0300

    loop/times
    mais estudo ruby

---
+++ loop.rb
@@ -0,0 +1,8 @@
+count = 2 
+  loop do 
+    puts count
+    if count == 15    
+        break 
+    end  
+    count +=1   #incrementa a variavel count
+  end
---
+++ times.rb
@@ -0,0 +1,3 @@
+10. times do           #10 é a quantidade de vezes que quero que repita (.times) comando para executar
+  puts"fazer a tarefa de casa"  
+end
