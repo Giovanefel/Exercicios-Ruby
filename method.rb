af77ee77dfedf71f807e1a08c39a8e714f0d9630
Giovane <giovanefe17@gmail.com>
2024-05-18 23:14:00 -0300

    Method
    Usando Metodo Def

---
+++ method.rb
@@ -0,0 +1,12 @@
+def talk (first_name, last_name)
+
+    puts "ola,  #{first_name} #{last_name} tubo bem?"
+
+ end
+
+ first_name = "giovane"
+ last_name = "felix"
+
+talk(first_name, last_name)
+
+
