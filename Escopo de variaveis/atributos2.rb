884ccc7a1c59462b2180b5872e5b9c3f1f31c4bf
Giovane <giovanefe17@gmail.com>
2024-05-22 21:03:34 -0300

    Uso atributos2
    Usando o atributo attr_acessor

---
+++ Escopo de variaveis/atributos2.rb
@@ -0,0 +1,10 @@
+class Dog 
+    attr_accessor :name, :age     #atributo 
+ end
+
+ dog = Dog.new                    # criado o objeto 
+ dog.name = 'Marlow'
+puts dog.name
+
+dog.age = ' 1 ano'
+puts dog.age 
