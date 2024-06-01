598ef9cb020dc9c0417bef301a0b3cd322c223af
Giovane <giovanefe17@gmail.com>
2024-06-01 20:52:23 -0300

    Namespace
    chama nome|frase invertidas

---
+++ RubyAv8/namespace.rb
@@ -0,0 +1,16 @@
+#EX
+#1
+module ReverseWorld
+   def self . puts (text)
+        print text.reverse.to_s
+    end
+end
+
+ReverseWorld::puts ' O resustado é sfsdfsdfs'  #nao leva espaçoi entre os :: 
+
+#a frase é chamada de forma invertida 
+
+#puts 'O resultado é .. '
+
+#podendo ser criada classe, variaveis
+
