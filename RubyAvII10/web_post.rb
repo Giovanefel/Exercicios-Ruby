8d5532046bdb7596a35c2ccd2ffde407793b6631
Giovane <101640518+Giovanefel@users.noreply.github.com>
2024-06-13 02:51:17 +0000

    web_post
    cont..

--- RubyAvII10/web_post.rb
+++ RubyAvII10/web_post.rb
@@ -1,4 +1,11 @@
 require 'net/http'
 
 req = Net::HTTP::Post.new("/api/users")
-#
+# para fazer chamadas https
+req.set_form_data({name:'Mario', job: 'Pintor'})
+
+response = Net::HTTP.start('reqress.in', use_ssl:true) do |https| https.request(req)
+end
+
+puts response.massage
+puts response.body
