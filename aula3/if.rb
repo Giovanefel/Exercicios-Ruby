ee69760046ca92f7ba8ffafc21d89653a2e644a0
giovanefel <giovanefe17@gmail.com>
2024-07-13 20:39:34 -0300

    arrumar

--- aula3/if.rb
+++ aula3/if.rb
@@ -5,4 +5,5 @@ if day == 'Sunday'   # == igual    != nao igual
     lunch = 'RotDog'
 end
 
-puts "lunch is #{lunch } today"
+puts "lunch is #{lunch } today"  # o #{ } mostra dentro das chaves a variavel  do lunch
+
