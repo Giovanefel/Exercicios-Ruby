e6dda7eea229b9bf1580a79c5637292102ee255b
giovanefel <giovanefe17@gmail.com>
2024-07-17 23:27:16 -0300

    contaula3

---
+++ aula3/loop.rb
@@ -0,0 +1,11 @@
+count = 8
+
+  loop do                
+      puts count
+      if count == 15    
+        break 
+      end  
+
+    count +=1 #incrementa a variavel count
+
+  end
---
+++ aula3/times.rb
@@ -0,0 +1,12 @@
+3. times do           #3 é a quantidade de vezes que quero que repita (.times) comando para executar
+  puts"fazer a tarefa de casa"  
+end
+
+1. times do
+  puts "estudar todo dia"
+
+end 
+
+2. times do
+  puts " vamos mais uma vez"
+end
